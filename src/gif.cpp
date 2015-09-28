f1e87f2dc7831633f2c0f9c9fb59ef9d56397822
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2015-09-28 13:57:17 +0200

    Manage case of empty GIF

--- src/gif.cpp
+++ src/gif.cpp
@@ -48,7 +48,7 @@ namespace lib_gif
   //----------------------------------------------------------------------------
   void gif::write(std::ofstream & p_file)
   {
-    if(gif_data_block::t_gif_data_block_type::TRAILER != m_data_blocks.back()->get_type())
+    if(!m_data_blocks.size() || gif_data_block::t_gif_data_block_type::TRAILER != m_data_blocks.back()->get_type())
       {
         m_data_blocks.push_back(new gif_trailer());
       }
